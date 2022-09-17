52c465f7fef30c12752b450f1f11e6bcfd32b2a4
David Cruwys <david@ideasmen.com.au>
2022-09-17 17:25:21 +1000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://appydave.com',
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), mdx()]
+  }), mdx(), sitemap()]
 });
---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'AppyDave';
+export const SITE_DESCRIPTION = 'Software Architect, Fullstack / Micro App Developer and Dancer';
